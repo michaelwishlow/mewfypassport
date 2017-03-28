fe08b43e5cb4d99fb2d679aab4ee82d33a4d2b42
michaelwishlow <michael@pantheritconsulting.com>
2017-03-28 15:36:54 -0400

    finish app.js

--- app.js
+++ app.js
@@ -10,3 +10,77 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mewfypassport');
+var db = mongoose.connection;
+
+//Routes
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+		secret: 'secret',
+		saveUninitialized: true,
+		resave: true
+}));
+
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express Validatator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect Flash
+app.use(flash());
+
+// Flash Global Variables
+app.use(function (req, res, next) {
+		res.locals.success_msg = req.flash('success_msg');
+		res.locals.error_msg = req.flash('error_msg');
+		res.locals.error = req.flash('error');
+		next();
+});
+
+app.use('/', routes);
+app.use('/users',users);
+
+//Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+		console.log('Server started on port '+app.get('port'));
+});
