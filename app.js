b6eada9fc3aecd68c63dab344ed47eee81ee4be2
michaelwishlow <michael@pantheritconsulting.com>
2017-03-31 00:06:29 -0400

    Routes

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mewfypassport');
-var db = mongoose.connection;
 
-//Routes
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,26 +26,25 @@ app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
 // BodyParser Middleware
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Express Session Middleware
+// Express Session
 app.use(session({
-		secret: 'secret',
-		saveUninitialized: true,
-		resave: true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
-// Passport Init
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Express Validatator
+// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -64,23 +62,26 @@ app.use(expressValidator({
   }
 }));
 
-//Connect Flash
+// Connect Flash
 app.use(flash());
 
-// Flash Global Variables
+// Global Vars
 app.use(function (req, res, next) {
-		res.locals.success_msg = req.flash('success_msg');
-		res.locals.error_msg = req.flash('error_msg');
-		res.locals.error = req.flash('error');
-		next();
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
-app.use('/', routes);
-app.use('/users',users);
 
-//Set Port
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
-		console.log('Server started on port '+app.get('port'));
+  console.log('Server started on port '+app.get('port'));
 });
